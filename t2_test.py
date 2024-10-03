4d5b31719f52e2d47aa6648c89f601895ff894c0
jehongmin <jehong.min@uqconnect.edu.au>
2024-10-03 18:54:09 +1000

    Update t2_test.py

--- t2_test.py
+++ t2_test.py
@@ -1,53 +1,21 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LogisticRegression, LinearRegression, Lasso
+from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import NearestNeighbors
-from sklearn.impute import KNNImputer
-from sklearn.preprocessing import StandardScaler
 import statsmodels.api as sm
-import seaborn as sns
-from pysyncon import Dataprep, Synth
-import cvxpy as cp
-
-
-"""
-Change file path (wdi.csv) to yours
-
-runner lines are at the bottom
-
-enable line 98 for the plots
-"""
+from preprocessing_checks import prepare_df
 
 def diffInDiff(agreement):
     if agreement == "CPTPP":
-        countries = ["Australia", "Canada", "Japan", "Mexico", "Vietnam"]
+        countries = ["Australia", "Canada", "Japan", "Mexico"]
         implementation_year = 2020
     elif agreement == "CETA":
-        countries = ["France", "Ireland", "Spain", "Italy", "Poland"]
-        implementation_year = 2019
-    else:
-        countries = ["Gambia", "Ghana",  "Mali", "Nigeria", "Senegal"]
-        implementation_year = 2016
+        countries = ["Ireland", "Spain", "Poland", "Germany"]
+        implementation_year = 2020
     
-    staging_df = pd.read_csv("wdi.csv")
-    df = pd.DataFrame() 
-
-    for yr in range(2008, 2024): # df is all of 2008 then all of 2009 etc.
-        temp_df = pd.DataFrame()
-        temp_df.insert(0, "Country Name", staging_df["Country Name"].unique())
-        temp_df.insert(1, "Year", yr)
-        for i, col in enumerate(staging_df["Series Name"].unique()):
-            temp_df.insert(i+2, col, list(staging_df[staging_df["Series Name"] == col].loc[:, str(yr)]))
-        df = pd.concat([df, temp_df])
-        
-
+    df = prepare_df("wdi.csv")
     df.insert(len(df.columns), "Treatment", [1 if item in countries else 0 for item in df["Country Name"]])
-
-    # scaler = StandardScaler() # scaling for some reason breaks it - can i scale for imputation, then unimpute
-    # df.iloc[:, 1:] = scaler.fit_transform(df.iloc[:, 1:]) 
-    imp = KNNImputer(n_neighbors=5)  # <- take 5 most similar samples and impute absed on that 
-    df.iloc[:, 1:] = imp.fit_transform(df.iloc[:, 1:])
 
     covariates = ['GDP (constant 2015 US$)', 'Trade (% of GDP)'
             ,'External balance on goods and services (% of GDP)']
@@ -78,26 +46,27 @@ def diffInDiff(agreement):
     #     plt.show()
 
     def did(df, df2, target):
-        # Step 4: Apply Difference-in-Differences (DiD)
         df['Post'] = np.where(df['Year'] >= implementation_year, 1, 0)
         df['interaction'] = df['Treatment'] * df['Post']
         X = sm.add_constant(df[['Treatment', 'Post', 'interaction']])
         y = df[target]  # Outcome of interest (e.g., trade volume)
         DiD_model = sm.OLS(y, X).fit()
-        # print(DiD_model.summary())
 
         if DiD_model.pvalues['interaction'] <= 0.05:
-            t_ys = np.array(df2[df2["Treatment"] == 1].loc[:, ['Year', target]].groupby(['Year']).mean().iloc[:, 0])
-            c_ys = np.array(df2[df2["Treatment"] == 0].loc[:, ['Year', target]].groupby(['Year']).mean().iloc[:, 0])
-            plt.plot(list(range(2008, 2024)), t_ys, color='r', label='CPTPP Countries')
-            plt.plot(list(range(2008, (2008 + len(c_ys)))), c_ys, color='b', label='non-CPTPP Countries')
-            plt.vlines(2019, ymin=min(min(t_ys), min(c_ys)), ymax=max(max(t_ys), max(c_ys)), linestyle='--', color='gray', label='CPTPP Implementation')
+            print(DiD_model.summary())
+            t_ys = np.array(df2[df2["Treatment"] == 1].loc[:, ['Year', target]].groupby(['Year']).mean().iloc[:, 0])[9:]
+            c_ys = np.array(df2[df2["Treatment"] == 0].loc[:, ['Year', target]].groupby(['Year']).mean().iloc[:, 0])[9:]
+            plt.style.use('seaborn-v0_8')
+            plt.plot(list(range(2008, 2024))[9:], t_ys, color='r', label='CPTPP Countries', linewidth=2)
+            plt.plot(list(range(2017, (2017 + len(c_ys)))), c_ys, color='b', label='non-CPTPP Countries', linewidth=2)
+            plt.vlines(implementation_year, ymin=min(min(t_ys), min(c_ys)), ymax=max(max(t_ys), max(c_ys)), linestyle='--', color='gray', label='CPTPP Implementation')
             
             plt.title(f"{target} by year of CPTPP and non-CPTPP countries")
             plt.xlabel('Year')
             plt.ylabel(target)
             plt.legend()
-            # plt.show()
+            
+            plt.show()
 
         if DiD_model.pvalues['interaction'] <= 0.05:
             print(f"{target}={np.round(DiD_model.pvalues['interaction'], 3)}")
@@ -105,9 +74,8 @@ def diffInDiff(agreement):
         else:
             return None
 
-    targets = staging_df["Series Name"].unique()
     hits = []
-    for x in targets:
+    for x in df.iloc[:, 2:-3].columns:
         ans = did(matched_data, df, x)
         if ans != None:
             hits.append(ans)
@@ -115,7 +83,5 @@ def diffInDiff(agreement):
     print(f"\n{len(hits)} variables are statistically significant\n")
     return(hits)
 
-# targets = diffInDiff("CETA")
-# targets = diffInDiff("CPTPP")
-
-
+# print(diffInDiff("CETA"))
+print(diffInDiff("CPTPP"))
